a96f66eb2ba9494cea5bf872e9407d96819bada4
OmurKamilK <omurkygsz@hotmail.com>
2024-04-11 14:52:07 +0300

    Update

--- SQLSolutions.sql
+++ SQLSolutions.sql
@@ -26,3 +26,11 @@
 
 --select CITY,STATE from STATION 
 
+--8-)Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
+
+--select distinct CITY from STATION where ID%2=0
+
+--9-)Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+
+--select count(CITY)- count(distinct CITY) from STATION  
+
